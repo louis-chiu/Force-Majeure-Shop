0ebddcb5462693f6b3ca83ad92cb0cbbf32106a3
Louis Chiu <s951080603@gmail.com>
2023-07-17 06:09:08 +0800

    Modify label names in the navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -4,27 +4,27 @@ import { FaBars } from 'react-icons/fa';
 import { navItems } from '../data';
 import { useRef, useState } from 'react';
 const Navbar = () => {
-  const groupRef = useRef(null);
-  const [showNavGroup, setShowNavGroup] = useState(false);
+  const linksRef = useRef(null);
+  const [showNavLinks, setShowNavLinks] = useState(false);
   const showStyle = {
-    height: showNavGroup
-      ? `${groupRef.current.getBoundingClientRect().height}px`
+    height: showNavLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
       : '0px',
   };
-  const toggleNavGroup = (e) => {
-    setShowNavGroup(!showNavGroup);
+  const toggleNavLinks = (e) => {
+    setShowNavLinks(!showNavLinks);
     console.log(showStyle);
   };
   return (
     <nav className='navbar'>
       <div className='navbar__header'>
         <span className='navbar__logo'>Force Majeure</span>
-        <button className='navbar__toggle' onClick={toggleNavGroup}>
+        <button className='navbar__toggle' onClick={toggleNavLinks}>
           <FaBars />
         </button>
       </div>
-      <div className='group-container' style={showStyle}>
-        <ul className='navbar__group' ref={groupRef}>
+      <div className='links-container' style={showStyle}>
+        <ul className='navbar__links' ref={linksRef}>
           {navItems.map((item, i) => {
             const { name, id, link, icon } = item;
             return (
