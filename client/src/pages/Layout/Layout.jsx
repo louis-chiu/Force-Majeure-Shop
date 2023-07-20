b205080fd4a280e6b40038c02f6c097037073897
Louis Chiu <s951080603@gmail.com>
2023-07-20 19:13:33 +0800

    Add path checking logic for optional rendering of FootBar

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,13 +1,16 @@
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import './Layout.scss';
 const Layout = () => {
+  const { pathname } = useLocation();
+  const memberPath = ['/login', '/signup'];
+  const isMemberPath = !memberPath.includes(pathname);
   return (
     <div className='layout-container'>
       <Navbar />
       <Outlet />
-      <Footer />
+      {isMemberPath && <Footer />}
     </div>
   );
 };
