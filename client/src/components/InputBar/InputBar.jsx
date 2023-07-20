01c9cf3a64c1f57373d3d741fab4e68ec41dbb78
Louis Chiu <s951080603@gmail.com>
2023-07-20 19:12:13 +0800

    Add InputBar component

--- client/src/components/InputBar/InputBar.jsx
+++ client/src/components/InputBar/InputBar.jsx
@@ -1,8 +1,31 @@
-const InputBar = ({ type }) => {
+import { useLocation } from 'react-router-dom';
+import './InputBar.scss';
+const InputBar = ({ type, name, children }) => {
+  const { pathname } = useLocation();
+  const requiredList = ['email', 'password'];
+  console.log(typeof children);
+  const labelStyle =
+    pathname === '/login'
+      ? {
+          fontSize: '2rem',
+          width: 'fit-content',
+        }
+      : {};
   return (
     <div className='input-bar'>
-      <div className='input-bar__name'></div>
-      <input type={type} />
+      <label
+        className='input-bar__label'
+        style={labelStyle}
+        htmlFor={name}
+      >
+        {children}
+      </label>
+      <input
+        className='input-bar__input'
+        type={type}
+        name={typeof children === 'string' ? children : type}
+        required={requiredList.includes(type)}
+      />
     </div>
   );
 };
