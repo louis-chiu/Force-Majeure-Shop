73152aecf8b7a3e74ac31ccb4686f373e9b2e313
Louis Chiu <s951080603@gmail.com>
2023-07-20 14:41:36 +0800

    Add AmountSelector component

--- client/src/components/AmountSelector.jsx
+++ client/src/components/AmountSelector.jsx
@@ -1,6 +1,17 @@
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
-
-const AmountSelector = () => {
-  return <div className='amount-selector'>AmountSelector</div>;
+import './AmountSelector.scss';
+const AmountSelector = ({ stock }) => {
+  let amount = 0;
+  return (
+    <div className='amount-selector'>
+      <button className='amount-selector__btn'>
+        <AiOutlineMinus />
+      </button>
+      <p className='amount-selector__amount'>{amount}</p>
+      <button className='amount-selector__btn'>
+        <AiOutlinePlus />
+      </button>
+    </div>
+  );
 };
 export default AmountSelector;
