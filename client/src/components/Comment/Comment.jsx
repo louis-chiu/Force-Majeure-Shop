1f1535edb6cacaa0189009183af09f86e5317b64
Louis Chiu <s951080603@gmail.com>
2023-07-25 01:12:10 +0800

    Add comment component and adjust single product page

--- client/src/components/Comment/Comment.jsx
+++ client/src/components/Comment/Comment.jsx
@@ -1,23 +1,44 @@
-const Comment = ({ image, name, rating, content, order }) => {
+import { defaultAvatarUrl } from '../../data';
+import './Comment.scss';
+const Comment = ({
+  image,
+  name: username,
+  rating,
+  content,
+  orderItem: {
+    size,
+    color: { hex, name: colorName },
+    amount,
+  },
+}) => {
   return (
     <div className='comment'>
-      <img
-        src={image}
-        alt={`${name}-avatar`}
-        className='comment__avatar'
-      />
+      <div className='comment__avatar-container'>
+        <img
+          src={image || defaultAvatarUrl}
+          alt={`${username}-avatar`}
+          className='comment__avatar'
+        />
+      </div>
       <div className='comment-body'>
-        <p className='comment__username'>{name}</p>
-        <p className='comment__content'>{content}</p>
+        <div className='comment__user-container'>
+          <p className='comment__username'>{username}</p>
+          <div className='comment__product-rating'>{rating}</div>
+        </div>
+        <div className='comment__content-container'>
+          <p className='comment__content'>{content}</p>
+        </div>
       </div>
-      <div className='comment__product-rating'></div>
       <div className='order-detail'>
-        <p className='order-detail__amount'>{order?.amount || 'amount'}</p>
-        <span className='order-detail__color'>{order?.color || 'color'}</span>
-        <span className='order-detail__size'>{order?.size || 'size'}</span>
+        <p className='order-detail__amount'>{`Buy  ${amount}` ?? 'amount'}</p>
+        <div
+          className='order-detail__color'
+          style={{ background: hex }}
+        />
+        <div className='order-detail__size'>{size ?? 'size'}</div>
       </div>
-      <div className='comment__edit'>edit</div>
 
+      {/* <div className='comment__edit'>edit</div> */}
     </div>
   );
 };
--- client/src/pages/SingleProduct/SingleProduct.jsx
+++ client/src/pages/SingleProduct/SingleProduct.jsx
@@ -1,21 +1,27 @@
 import { useParams } from 'react-router-dom';
 import ProductForm from '../../components/ProductForm/ProductForm';
-import { useGetProductByIdQuery } from '../../services/product';
+import { useGetProductByIdQuery } from '../../features/product/productSlice';
 import Error from '../Error/Error.jsx';
 import Loading from '../Loading/Loading';
 import './SingleProduct.scss';
+import Comment from '../../components/Comment/Comment';
 
 const SingleProduct = () => {
   const { id } = useParams();
   const { data: product, isError, isLoading } = useGetProductByIdQuery(id);
-
+  const commentList = product?.comments;
   if (isError) return <Error />;
   if (isLoading) return <Loading />;
 
-  console.log(product);
   return (
     <div className='single-product'>
       <ProductForm {...product} />
+      <section className='comment-list'>
+        {commentList?.map((comment) => {
+          console.log(comment);
+          return <Comment {...comment} />;
+        })}
+      </section>
     </div>
   );
 };
