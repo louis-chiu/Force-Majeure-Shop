adefd864cbebcdc87d1dcd26abce313fc699aa04
Louis Chiu <s951080603@gmail.com>
2023-07-28 01:05:33 +0800

    Update Rating component to display customer rating

--- client/src/components/Rating/Rating.jsx
+++ client/src/components/Rating/Rating.jsx
@@ -1,6 +1,23 @@
 import './Rating.scss';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
-const Rating = () => {
-  return <div>Rating</div>;
+const Rating = ({ rating }) => {
+  const starArray = [0, 0, 0, 0, 0];
+  const hasHalfStar = rating % 1 !== 0;
+  const numOfFillStar = Math.floor(rating);
+  const numOfEmptyStar = Math.floor(5 - rating);
+  return (
+    <div className='rating'>
+      {starArray.map((type, index) => {
+        if (index < numOfFillStar) {
+          return <BsStarFill key={index} />;
+        } else if (hasHalfStar && index == numOfFillStar) {
+          return <BsStarHalf key={index} />;
+        } else {
+          return <BsStar key={index} />;
+        }
+      })}
+    </div>
+  );
 };
 export default Rating;
