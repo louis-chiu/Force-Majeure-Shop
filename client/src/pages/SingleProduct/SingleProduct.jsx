14d088249d7f46f296aae1ef3347c2857a58fadd
Louis Chiu <s951080603@gmail.com>
2023-07-28 01:11:56 +0800

    Add loader to reset cart item states before changing product route

--- client/src/pages/SingleProduct/SingleProduct.jsx
+++ client/src/pages/SingleProduct/SingleProduct.jsx
@@ -1,25 +1,54 @@
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
 import ProductForm from '../../components/ProductForm/ProductForm';
-import { useGetProductByIdQuery } from '../../features/product/productSlice';
+import productSlice, {
+  useGetProductByIdQuery,
+} from '../../features/product/productSlice';
 import Error from '../Error/Error.jsx';
 import Loading from '../Loading/Loading';
 import './SingleProduct.scss';
 import Comment from '../../components/Comment/Comment';
+import { useDispatch } from 'react-redux';
+import { resetCartItem } from '../../features/cartItem/cartItemSlice';
+import { store } from '../../store';
 
+import { extendedApiSlice as productApi } from '../../features/product/productSlice';
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const {
+    data: product,
+    isError,
+    isLoading,
+  } = await store.dispatch(productApi.endpoints.getProductById.initiate(id));
+  store.dispatch(
+    resetCartItem({
+      name: product.name,
+      image: product.image[0],
+      price: product.price,
+      color: product.color[0].name,
+      size: product.size[0],
+      id,
+    })
+  );
+  return { data: product, isError, isLoading };
+};
 const SingleProduct = () => {
-  const { id } = useParams();
-  const { data: product, isError, isLoading } = useGetProductByIdQuery(id);
+  const { data: product, isError, isLoading } = useLoaderData();
+
   const commentList = product?.comments;
   if (isError) return <Error />;
   if (isLoading) return <Loading />;
 
   return (
     <div className='single-product'>
-      <ProductForm {...product} />
+      <ProductForm product={product} />
       <section className='comment-list'>
         {commentList?.map((comment) => {
-          console.log(comment);
-          return <Comment {...comment} />;
+          return (
+            <Comment
+              key={comment.commentId}
+              {...comment}
+            />
+          );
         })}
       </section>
     </div>
