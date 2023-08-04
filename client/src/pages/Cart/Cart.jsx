d4b5365f6ce5184d0b77c68da0640eab4207d587
Louis Chiu <s951080603@gmail.com>
2023-08-04 23:47:48 +0800

    Add shopping cart feature

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,6 +1,16 @@
+import './Cart.scss';
+import CartList from '../../components/CartList/CartList';
+import CheckoutBar from '../../components/CheckoutBar/CheckoutBar';
+
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
-export default Cart
+    <div className='cart'>
+      <div className='cart__container'>
+        <h2 className='cart__title'>Shopping Cart</h2>
+        <CartList />
+      </div>
+      <CheckoutBar />
+    </div>
+  );
+};
+export default Cart;
