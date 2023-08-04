188e2e79b2443e60f56f31ff834de2b889acc351
Louis Chiu <s951080603@gmail.com>
2023-08-04 23:54:52 +0800

    Move the constant data to data,jsx

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -46,6 +46,13 @@ export const socialMedia = [
   },
 ];
 
-export const productList = [{
-  
-}]
+export const bannerUrlList = [
+  'src/assets/banner1.jpg',
+  'src/assets/banner2.jpg',
+  'src/assets/banner3.jpg',
+];
+
+export const productList = [{}];
+
+export const defaultAvatarUrl =
+  'https://images.unsplash.com/photo-1571988840298-3b5301d5109b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80';
